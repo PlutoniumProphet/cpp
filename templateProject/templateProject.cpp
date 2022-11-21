8c2746646dc7fd0a54d3c4e4aa49605c4560f168
Vegator <vegator@tutanota.com>
2022-11-21 11:54:06 +0000

    cpp templatre created

---
+++ templateProject/templateProject.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    std::cin.get();
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+
